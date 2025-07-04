f45e8da307289889145e14d899c4484300ddc5f6
koralle <koralle@koralle-mgmg.com>
2025-07-04 23:36:43 +0900

    feat: run `astro add cloudflare`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,23 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://daily-tech-blog.pages.dev',
   integrations: [mdx(), sitemap()],
+
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
       wrap: true
     }
   },
+
   build: {
     format: 'file'
-  }
+  },
+
+  adapter: cloudflare()
 });
