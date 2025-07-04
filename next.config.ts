84015343d14d2a27797303be179853ae09487a5f
koralle <koralle@koralle-mgmg.com>
2025-07-04 22:32:52 +0900

    feat: setup `rehype-pretty-code`

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from 'next';
 import createMDX from '@next/mdx';
+import { rehypePrettyCode } from 'rehype-pretty-code';
 
 const nextConfig: NextConfig = {
   /* config options here */
   pageExtensions: ['tsx', 'mdx']
 };
 
-const withMDX = createMDX();
+const withMDX = createMDX({
+  options: {
+    rehypePlugins: [[rehypePrettyCode]]
+  }
+});
 const wrappedNextConfig = withMDX(nextConfig);
 
 export default wrappedNextConfig;
