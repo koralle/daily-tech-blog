49851f165bd6d43ae266c163db87cf721b9aae94
koralle <koralle@koralle-mgmg.com>
2025-07-05 01:13:54 +0900

    feat: set language Japanese

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Daily Tech Blog';
+export const SITE_DESCRIPTION = '技術に関する日々の学びと気づきを記録するブログ';
