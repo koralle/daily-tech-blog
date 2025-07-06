eb32149c8aba26321d3e4bd3fcdd603d12645715
koralle <koralle@koralle-mgmg.com>
2025-07-07 06:33:08 +0900

    feat(lint): use ESLint and Oxlint

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,6 @@ import type { Env } from 'worker-configuration.d.ts';
 type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
 
 declare namespace App {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface Locals extends Runtime {
     // Add any custom properties here if needed
   }
