6ab57b186f2edefaf47151aede52b42ad4a69430
koralle <koralle@koralle-mgmg.com>
2025-07-07 01:59:10 +0900

    feat: resolve ESLint errors

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,10 @@
+import type { Env } from 'worker-configuration.d.ts';
+
 type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
 
 declare namespace App {
-  interface Locals extends Runtime {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface Locals extends Runtime {
+    // Add any custom properties here if needed
+  }
 }
