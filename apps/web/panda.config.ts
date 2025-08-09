5dcfee66df97d4f09113319b2420ee126024a7f1
koralle <koralle@koralle-mgmg.com>
2025-08-10 02:24:10 +0900

    feat(web/pandacss): define color-tokens `rose-pink`

--- apps/web/panda.config.ts
+++ apps/web/panda.config.ts
@@ -14,7 +14,31 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {}
+    extend: {
+      tokens: {
+        colors: {
+          primary: {
+            value: '{colors.rosePink.600}',
+            description: 'koralle\'s brand color'
+          },
+          rosePink: {
+            50: { value: '#FEFCFD' },
+            100: { value: '#FDF8F9' },
+            200: { value: '#FAEEF0' },
+            300: { value: '#F7DFE3' },
+            400: { value: '#F4CFD6' },
+            500: { value: '#F1B1BD' },
+            600: { value: '#EC93A1' },
+            700: { value: '#D47585' },
+            800: { value: '#BC5769' },
+            900: { value: '#983D4E' },
+            1000: { value: '#742E3A' },
+            1100: { value: '#4A1D25' },
+            1200: { value: '#2E0F13' }
+          }
+        }
+      }
+    }
   },
 
   // The output directory for your css system
