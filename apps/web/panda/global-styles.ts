d50b0432dd210ed622621c196d0012bbf35d985a
koralle <koralle@koralle-mgmg.com>
2025-08-12 01:14:57 +0900

    feat(web/panda): define global-fonts

---
+++ apps/web/panda/global-styles.ts
@@ -0,0 +1,8 @@
+import { defineGlobalStyles } from '@pandacss/dev';
+
+export const globalStyles = defineGlobalStyles({
+  html: {
+    '--global-font-body': 'fonts.sans',
+    '--global-font-mono': 'fonts.mono'
+  }
+});
