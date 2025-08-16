2db690d8408e4cfdc80b2a81545d013034026025
koralle <koralle@koralle-mgmg.com>
2025-08-16 12:34:56 +0900

    feat: update global styles

--- apps/web/panda/global-styles.ts
+++ apps/web/panda/global-styles.ts
@@ -4,7 +4,23 @@ export const globalStyles = defineGlobalStyles({
   html: {
     '--global-font-body': 'fonts.sans',
     '--global-font-mono': 'fonts.mono',
-    backgroundColor: 'color-mix(in oklch, var(--colors-rose-pink-100) 50%, transparent)'
+    backgroundColor: 'color-mix(in oklch, var(--colors-rose-pink-100) 50%, transparent)',
+
+    // 東アジアの文字の字形を可変幅で表示する
+    // https://ics.media/entry/250401/#font-variant-east-asian
+    fontVariantEastAsian: 'proportional-width',
+
+    // 行ボックスに収まらない時に折り返すかどうか
+    // https://ics.media/entry/240411/#overflow-wrap
+    overflowWrap: 'anywhere',
+
+    // 文字内の単語がどの位置で折り返されるかを指定する
+    // https://ics.media/entry/240411/#word-break
+    wordBreak: 'auto-phrase',
+
+    // 禁則処理
+    // https://ics.media/entry/240411/#line-break
+    lineBreak: 'strict'
   },
   ':where(.astro-code)': {
     padding: '32px',
