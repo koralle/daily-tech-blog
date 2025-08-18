701c991651b32e86e749be5be6eca8a6250d65e0
koralle <koralle@koralle-mgmg.com>
2025-08-18 22:38:08 +0900

    feat(web/panda): use `:where(html)` instead of `html`

--- apps/web/panda/global-styles.ts
+++ apps/web/panda/global-styles.ts
@@ -1,7 +1,7 @@
 import { defineGlobalStyles } from '@pandacss/dev';
 
 export const globalStyles = defineGlobalStyles({
-  html: {
+  ':where(html)': {
     '--global-font-body': 'fonts.sans',
     '--global-font-mono': 'fonts.mono',
     backgroundColor: 'color-mix(in oklch, var(--colors-rose-pink-100) 50%, transparent)',
