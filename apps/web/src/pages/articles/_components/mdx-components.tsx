362d08ff64ce59f948381236aa1ca237ec718f8a
koralle <koralle@koralle-mgmg.com>
2025-08-20 17:00:55 +0900

    feat(web/mdx): implement `MDX` components

---
+++ apps/web/src/pages/articles/_components/mdx-components.tsx
@@ -0,0 +1,232 @@
+import { css } from '../../../../styled-system/css';
+import type { ComponentProps, JSX } from 'solid-js';
+import { splitProps } from 'solid-js';
+
+export interface MDXComponentProps {
+  children: JSX.Element;
+}
+
+interface LinkProps extends ComponentProps<'a'> {
+  children: JSX.Element;
+}
+
+const Link = (props: LinkProps) => {
+  const [local, rest] = splitProps(props, ['children', 'class']);
+
+  return (
+    <a
+      target="_blank"
+      {...rest}
+      class={css({
+        color: 'rosePink.700',
+        textDecoration: 'underline',
+        textUnderlineOffset: '3',
+        padding: '0.25em',
+        borderRadius: '4',
+        _hover: {
+          backgroundColor: 'color-mix(in oklch, var(--colors-rose-pink-600) 12.5%, transparent)'
+        },
+        _active: {
+          backgroundColor: 'color-mix(in oklch, var(--colors-rose-pink-600) 25%, transparent)'
+        },
+        _focusVisible: {
+          outlineOffset: '2'
+        },
+        _visited: {
+          color: 'rosePink.900'
+        }
+      })}
+    >
+      {local.children}
+    </a>
+  );
+};
+
+const H2 = ({ children }: MDXComponentProps) => (
+  <h2
+    class={css({
+      color: 'gray.700',
+      fontSize: '24',
+      fontWeight: '700',
+      letterSpacing: '0.04em',
+      lineHeight: '150'
+    })}
+  >
+    {children}
+  </h2>
+);
+
+const P = ({ children }: MDXComponentProps) => (
+  <p
+    class={css({
+      color: 'gray.800',
+      fontSize: '16',
+      fontWeight: '400',
+      letterSpacing: '0.04em',
+      lineHeight: '170'
+    })}
+  >
+    {children}
+  </p>
+);
+
+const Code = ({ children }: MDXComponentProps) => (
+  <code
+    class={css({
+      '&:where(:not(:has(.line)))': {
+        color: 'rosePink.600',
+        fontWeight: '700',
+        backgroundColor: 'color-mix(in oklch, var(--colors-rose-pink-600) 12.5%, transparent)',
+        padding: '0.25em',
+        borderRadius: '4',
+        letterSpacing: '0.025em'
+      }
+    })}
+  >
+    {children}
+  </code>
+);
+
+const Strong = ({ children }: MDXComponentProps) => (
+  <strong
+    class={css({
+      color: 'gray.700',
+      fontSize: '16',
+      fontWeight: '700',
+      letterSpacing: '0.025em'
+    })}
+  >
+    {children}
+  </strong>
+);
+
+interface TableProps extends ComponentProps<'table'> {
+  children: JSX.Element;
+}
+
+const Table = (props: TableProps) => {
+  const [local, rest] = splitProps(props, ['children', 'class']);
+
+  return (
+    <table
+      class={css({
+        width: '100%',
+        md: {
+          width: '90%'
+        },
+        alignSelf: 'center',
+        boxSizing: 'border-box',
+        borderCollapse: 'collapse',
+        border: '1px solid var(--colors-gray-100)',
+        overflow: 'scroll'
+      })}
+      {...rest}
+    >
+      {local.children}
+    </table>
+  );
+};
+
+interface TdProps extends ComponentProps<'td'> {
+  children: JSX.Element;
+}
+
+const Td = (props: TdProps) => {
+  const [local, rest] = splitProps(props, ['children', 'class', 'style']);
+
+  return (
+    <td
+      style={local.style}
+      class={css({
+        textAlign: 'start',
+        paddingX: 'calc(var(--spacing-unit) * 4)',
+        paddingY: 'calc(var(--spacing-unit) * 2)',
+        boxSizing: 'border-box',
+        border: '1px solid var(--colors-gray-200)'
+      })}
+      {...rest}
+    >
+      {local.children}
+    </td>
+  );
+};
+
+interface ThProps extends ComponentProps<'th'> {
+  children: JSX.Element;
+}
+
+const Th = (props: ThProps) => {
+  const [local, rest] = splitProps(props, ['children', 'class', 'style']);
+
+  if (local.style === 'textAlign:right') {
+    alert('foo');
+  }
+
+  return (
+    <th
+      style={local.style}
+      class={css({
+        color: 'gray.900',
+        backgroundColor: 'color-mix(in oklch, var(--colors-rose-pink-800) 20%, transparent)',
+        textAlign: 'start',
+        paddingX: 'calc(var(--spacing-unit) * 4)',
+        paddingY: 'calc(var(--spacing-unit) * 2)',
+        boxSizing: 'border-box',
+        border: '1px solid var(--colors-gray-200)'
+      })}
+      {...rest}
+    >
+      {local.children}
+    </th>
+  );
+};
+
+interface TrProps extends ComponentProps<'tr'> {
+  children: JSX.Element;
+}
+
+const Tr = (props: TrProps) => {
+  const [local, rest] = splitProps(props, ['children', 'class']);
+
+  return (
+    <tr
+      class={css({
+        width: '100%',
+        boxSizing: 'border-box',
+        border: '1px solid var(--colors-gray-100)',
+        '&:nth-child(even)': {
+          backgroundColor: 'color-mix(in oklch, var(--colors-rose-pink-800) 6.25%, transparent)'
+        }
+      })}
+      {...rest}
+    >
+      {local.children}
+    </tr>
+  );
+};
+
+const Ul = ({ children }: MDXComponentProps) => (
+  <ul
+    class={css({
+      color: 'gray.800',
+      listStyleType: 'circle',
+      listStylePosition: 'inside',
+      paddingX: 'calc(var(--spacing-unit) * 4)'
+    })}
+  >
+    {children}
+  </ul>
+);
+
+export const MDX = {
+  H2: H2,
+  Link: Link,
+  P: P,
+  Code: Code,
+  Strong: Strong,
+  Table: Table,
+  Th: Th,
+  Td: Td,
+  Tr: Tr,
+  Ul: Ul
+};
