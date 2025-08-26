007dd95a1b5002faea0e0b8e62f4410f6ee97d52
koralle <koralle@koralle-mgmg.com>
2025-08-27 00:44:47 +0900

    feat(web/tag): add many tags (#83)

--- apps/web/src/data/tags.ts
+++ apps/web/src/data/tags.ts
@@ -60,7 +60,66 @@ export const TAGS: readonly Tag[] = [
     id: 'neovim',
     displayName: 'Neovim',
     description: '最強のテキストエディタであるNeovimについて'
-  }
+  },
+  {
+    id: 'react',
+    displayName: 'React',
+    description: 'ユーザーインターフェースを構築するためのJavaScriptライブラリ'
+  },
+  {
+    id: 'solidjs',
+    displayName: 'SolidJS',
+    description: 'ユーザーインターフェースを構築するための宣言的なJavaScriptライブラリ'
+  },
+  { id: 'vue', displayName: 'Vue', description: 'プログレッシブJavaScriptフレームワーク' },
+  { id: 'svelte', displayName: 'Svelte', description: 'サイバネティックに強化されたWebアプリ' },
+  {
+    id: 'tanstack-router',
+    displayName: 'Tanstack Router',
+    description: 'ファーストクラスの検索パラメータAPIを備えた、完全に型安全なルーター'
+  },
+  {
+    id: 'git',
+    displayName: 'Git',
+    description: '無料のオープンソース分散バージョン管理システム'
+  },
+  {
+    id: 'github',
+    displayName: 'GitHub',
+    description: 'Gitを使用したソフトウェア開発とバージョン管理のためのインターネットホスティングプロバイダー'
+  },
+  {
+    id: 'dependabot',
+    displayName: 'Dependabot',
+    description: 'Dependabotに関するあれこれ'
+  },
+  { id: 'pnpm', displayName: 'pnpm', description: 'pnpmに関するあれこれ' },
+  {
+    id: 'rust',
+    displayName: 'Rust',
+    description: 'Rustに関するあれこれ'
+  },
+  {
+    id: 'github-actions',
+    displayName: 'GitHub Actions',
+    description: 'GitHub Actionsに関するあれこれ'
+  },
+  { id: 'security', displayName: 'セキュリティ', description: 'Webセキュリティに関するあれこれ' },
+  { id: 'network', displayName: 'ネットワーク', description: 'ネットワークに関するあれこれ' },
+  {
+    id: 'vitest',
+    displayName: 'Vitest',
+    description: 'Vitestに関するあれこれ'
+  },
+  {
+    id: 'storybook',
+    displayName: 'Storybook',
+    description: 'Storybookに関するあれこれ'
+  },
+  { id: 'algorithm', displayName: 'アルゴリズム', description: 'アルゴリズムに関するあれこれ' },
+  { id: 'database', displayName: 'データベース', description: 'データベースに関するあれこれ' },
+  { id: 'system-design', displayName: 'システムデザイン', description: 'システムデザインに関するあれこれ' },
+  { id: 'aws', displayName: 'AWS', description: 'アマゾンウェブサービス' }
 ] as const;
 
 // 型安全なタグID
