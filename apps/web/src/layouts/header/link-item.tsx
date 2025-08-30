6989555dae0f3a8f746c4259e090f9393d0faa2a
koralle <koralle@koralle-mgmg.com>
2025-08-30 14:44:07 +0900

    feat: update

---
+++ apps/web/src/layouts/header/link-item.tsx
@@ -0,0 +1,42 @@
+import { sva } from '../../../styled-system/css';
+import { createStyleContext } from '../../../styled-system/jsx';
+
+const linkItem = sva({
+  slots: ['root', 'icon', 'label'],
+  base: {
+    root: {
+      color: 'gray.700',
+      display: 'grid',
+      gridTemplateColumns: 'max-content 1fr',
+      alignItems: 'center',
+      backgroundColor: 'white',
+      gap: 'calc(var(--spacing-unit) * 4)',
+      height: 'calc(var(--spacing-unit) * 14)',
+      paddingInlineStart: 'calc(var(--spacing-unit) * 8)',
+      cursor: 'pointer',
+      _hover: {
+        backgroundColor: 'rosePink.100'
+      }
+    },
+    icon: {
+      fill: 'currentcolor'
+    },
+    label: {
+      fontSize: 16,
+      fontWeight: '700',
+      letterSpacing: '0.04em'
+    }
+  }
+});
+
+const { withProvider, withContext } = createStyleContext(linkItem);
+
+const Root = withProvider('a', 'root');
+const Icon = withContext('div', 'icon');
+const Label = withContext('span', 'label');
+
+export const LinkItem = {
+  Root,
+  Icon,
+  Label
+};
