26baf888558dfa25e2e39eb714d31219333aae03
koralle <koralle@koralle-mgmg.com>
2025-09-05 11:58:01 +0900

    chore(web/oxlint): update `env.d.ts`

--- apps/web/src/env.d.ts
+++ apps/web/src/env.d.ts
@@ -1,8 +1,11 @@
 /// <reference types="astro/client" />
+/* oxlint-disable typescript-eslint/no-empty-interface */
+/* oxlint-disable typescript-eslint/no-empty-object-type */
 
+import type { Runtime as CFRuntime } from '@astrojs/cloudflare';
 import type { Env } from 'worker-configuration.d.ts';
 
-type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+type Runtime = CFRuntime.Runtime<Env>;
 
 declare namespace App {
   interface Locals extends Runtime {
